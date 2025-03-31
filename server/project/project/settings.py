8daf16d824dacb788726561a5f8f0b0ddb8b243f
cjgreen3 <70037436+cjgreen3@users.noreply.github.com>
2025-03-31 16:30:06 -0600

    we got a working dockerfile woo

--- server/project/project/settings.py
+++ server/project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-sqe54#%2&ve2gxu3)781%r$7a-fmudd+x(pne0h&-q4p6zjz5d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
